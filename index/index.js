16498a2b11841f98569b3611b26f3e601cd089dd
WonYoungYun <asmar20288@naver.com>
2018-07-30 19:48:56 +0900

    library

---
+++ index/index.js
@@ -0,0 +1,6 @@
+function fetchPages(name,position){
+    fetch(name).then(function(response){response.text().then(function(text){
+        document.querySelector(position).innerHTML = text;
+        })
+    })
+}
